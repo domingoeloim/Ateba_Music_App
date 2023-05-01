4ae225be01be8f3e3574060086e031cbfd69f739
domingoeloim <eloimdomingo@gmail.com>
2023-05-01 12:01:45 +0000

    Updates

---
+++ precache-manifest.3f4f4dc58969dc112fce9a06e29ba03e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1d81c9c34a3fd6d6f10b3c8af7d57a",
+    "url": "/Ateba_Music_App/index.html"
+  },
+  {
+    "revision": "16c01292223c4e8f71fb",
+    "url": "/Ateba_Music_App/static/css/main.d12a6c17.chunk.css"
+  },
+  {
+    "revision": "997af168da847a91a3f4",
+    "url": "/Ateba_Music_App/static/js/2.d5ffd37f.chunk.js"
+  },
+  {
+    "revision": "e5999e63c33a32e77a192dc439bcb94e",
+    "url": "/Ateba_Music_App/static/js/2.d5ffd37f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16c01292223c4e8f71fb",
+    "url": "/Ateba_Music_App/static/js/main.cb36924a.chunk.js"
+  },
+  {
+    "revision": "b41db05b2ff77e5b0362",
+    "url": "/Ateba_Music_App/static/js/runtime-main.a5e3eb3b.js"
+  },
+  {
+    "revision": "adde56c18b09e9369f36cb20b4131a7d",
+    "url": "/Ateba_Music_App/static/media/DancingScript-VariableFont_wght.adde56c1.ttf"
+  }
+]);
